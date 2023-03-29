925c17a4d7f222a3494e70d8e5e7b18683a7a0b5
Jminx42 <visagiejessica@gmail.com>
2023-03-29 19:46:09 +0000

    made the basic layout via html and the basic code via js

--- src/app.js
+++ src/app.js
@@ -6,6 +6,24 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  const cardSuit = ["♥", "♦", "♣", "♠"];
+  const cardOther = ["A", "J", "Q", "K"];
+
+  let randomNumber = Math.floor(Math.random() * 13) + 1;
+  let cardSeeds = document.querySelectorAll(".card-seed");
+  let cardNumber = document.querySelector(".card-number");
+
+  if (randomNumber == 1) {
+    cardNumber.innerHTML = cardOther[0];
+  } else if (randomNumber == 11) {
+    cardNumber.innerHTML = cardOther[1];
+  } else if (randomNumber == 12) {
+    cardNumber.innerHTML = cardOther[2];
+  } else if (randomNumber == 13) {
+    cardNumber.innerHTML = cardOther[3];
+  } else cardNumber.innerHTML = randomNumber;
+
+  let randomNum = Math.floor(Math.random() * cardSuit.length);
+  cardSeeds[0].innerHTML = cardSuit[randomNum];
+  cardSeeds[1].innerHTML = cardSuit[randomNum];
 };
